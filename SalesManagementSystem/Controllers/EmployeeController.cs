94283ebebc02771e0d258dee35be42e80bb80aad
fuadfaruk <fuadfaruk707@gmail.com>
2026-05-13 22:38:28 +0600

    Add Employee CRUD endpoints with DTOs for input handling

--- SalesManagementSystem/Controllers/EmployeeController.cs
+++ SalesManagementSystem/Controllers/EmployeeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SalesManagementSystem.Data;
+using SalesManagementSystem.Dtos;
 using SalesManagementSystem.Models;
 
 namespace SalesManagementSystem.Controllers
@@ -26,6 +27,52 @@ namespace SalesManagementSystem.Controllers
         {
             var employee = _context.Employees.FirstOrDefault(e => e.emp_id == empId);
             return employee != null ? Ok(employee) : NotFound();
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployee(CreateEmployeeDto employeeDto)
+        {
+            var employee = new Employee
+            {
+                birth_date = employeeDto.birth_date,
+                first_name = employeeDto.first_name,
+                last_name = employeeDto.last_name,
+                salary = employeeDto.salary,
+                sex = employeeDto.sex
+            };
+            _context.Employees.Add(employee);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetEmployeeById), new { empId = employee.emp_id }, employee);
+        }
+
+        [HttpPut("{empId:int}")]
+        public IActionResult UpdateEmployee(int empId, UpdateEmployeeDto employeeDto)
+        {
+            var employee = _context.Employees.FirstOrDefault(e => e.emp_id == empId);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            employee.birth_date = employeeDto.birth_date;
+            employee.first_name = employeeDto.first_name;
+            employee.last_name = employeeDto.last_name;
+            employee.salary = employeeDto.salary;
+
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(UpdateEmployee), new { empId = employee.emp_id }, employee);
+        }
+
+        [HttpDelete("{empId:int}")]
+        public IActionResult DeleteEmployee(int empId)
+        {
+            var employee = _context.Employees.FirstOrDefault(e => e.emp_id == empId);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            _context.Employees.Remove(employee);
+            _context.SaveChanges();
+            return NoContent();
         }
     }
 }
---
+++ SalesManagementSystem/Dtos/UpdateEmployeeDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SalesManagementSystem.Dtos
+{
+    public class UpdateEmployeeDto
+    {
+        public DateOnly birth_date { get; set; }
+        public required string first_name { get; set; }
+        public string? last_name { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal salary { get; set; }
+        public string? sex { get; set; }
+    }
+}
